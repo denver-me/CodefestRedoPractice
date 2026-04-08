56cccadca33b086881c31ace6cc2d31fd6699682
denver6000 <denverballesteros01@gmail.com>
2026-04-08 10:37:10 +0800

    feat: Added Hilt and Room Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -36,11 +37,26 @@ android {
 
 dependencies {
 
+    val room_version = "2.8.4"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version" )
+    implementation("com.google.dagger:hilt-android:2.57.1")
+    annotationProcessor("com.google.dagger:hilt-android-compiler:2.57.1")
+
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
