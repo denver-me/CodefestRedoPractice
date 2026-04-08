ec06ac3a2225ca6bd2a3dd3cefdf624b4c4ff9a2
denver6000 <denverballesteros01@gmail.com>
2026-04-08 13:17:25 +0800

    feat: Rooms Management Initial

---
+++ app/src/main/java/xyz/denprog/codefestredopractice/admin/room_management/MyRoomsManagementRecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package xyz.denprog.codefestredopractice.admin.room_management;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import xyz.denprog.codefestredopractice.admin.room_management.placeholder.PlaceholderContent.PlaceholderItem;
+import xyz.denprog.codefestredopractice.databinding.FragmentItemBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyRoomsManagementRecyclerViewAdapter extends RecyclerView.Adapter<MyRoomsManagementRecyclerViewAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public MyRoomsManagementRecyclerViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentItemBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
